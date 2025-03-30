f0a5ffb2e7b700a3a83696aa297fe6a656f7e760
themysterysolver <arjunprabhakar1910@gmail.com>
2025-03-30 15:48:25 +0530

    FEAT:got board without wall

--- ZIP_EXTENSION/zip.js
+++ ZIP_EXTENSION/zip.js
@@ -7,5 +7,18 @@ document.getElementById("pop").addEventListener("click",async ()=>{
     })
 })
 function solveIt(){
-    console.log("Hello world!");
+    let iframe_coll=document.getElementsByClassName("game-launch-page__iframe w-full")[0];
+    let iframe=iframe_coll.contentDocument||iframe_coll.contentWindow?.document;
+    console.log(iframe);
+
+    let content=iframe.getElementsByClassName("trail-cell");
+    let idx_num=new Map();
+    Array.from(content).forEach((val,idx)=>{
+        let the_div=val.querySelector(".trail-cell-content");
+        if(the_div){
+            idx_num.set(idx,the_div.innerText);
+        }
+    })
+    console.log(idx_num)
+
 }
